6983fb6dbcabafe7bd25bb75369931c79651ecc0
karthigeyan <karthiasokan179@gmail.com>
2023-12-07 16:00:41 +0530

    search filter

---
+++ src/app/shared/components/dynamic-view-table/dynamic-view-table.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DynamicViewTableComponent } from './dynamic-view-table.component';
+
+describe('DynamicViewTableComponent', () => {
+  let component: DynamicViewTableComponent;
+  let fixture: ComponentFixture<DynamicViewTableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DynamicViewTableComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DynamicViewTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
