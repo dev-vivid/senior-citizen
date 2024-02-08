bc8de9a0310bd686acf68a9f2952758bd6c925ae
KarthiKrish <karthibca28@gmail.com>
2024-02-08 13:58:18 +0530

    Forgot password update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -13,12 +13,19 @@ import { SpinnrService } from 'src/app/shared/services/spinnr.service';
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   loginError: string = '';
-  emailId = new FormControl('', Validators.required);
-  display: boolean;
+  // emailId = new FormControl('', Validators.required);
   check:boolean;
   userRole: any;
   roleName: any;
   loader: boolean;
+  // Forgot password
+  display: boolean = false;
+  verificationDisplay: boolean = false;
+  changePasswordDisplay: boolean = false;
+  emailId: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  verificationCode: FormControl = new FormControl('', Validators.required);
+  newPassword: FormControl = new FormControl('', Validators.required);
+  confirmPassword: FormControl = new FormControl('', Validators.required);
 
   constructor(private authService: AuthService, private router: Router, private sharedService: SharedService, public spinnerService: SpinnrService) { }
 
@@ -65,14 +72,76 @@ export class LoginComponent implements OnInit {
   forgotPassword() {
     this.display = true;
   }
+
   submitFP() {
     if (this.emailId.valid) {
-
-    } else {
+      const data = {
+        "email": this.emailId.value
+      }
+      this.authService.ForgetPassword(data).subscribe((resp: any) => {
+        // console.log("Response submit", resp);
+        if(resp?.statusCode === 200){
+          this.sharedService.showSuccess(resp.message);
+          this.display = false;
+          this.verificationDisplay = true;
+        }else{
+          this.sharedService.showError(resp.message);
+        }
+      },(e:any)=>{
+          this.sharedService.showError(e?.message);
+      });
+    }else {
       this.emailId.markAsTouched();
     }
   }
+  verifyEmail() {
+    if (this.verificationCode.valid) {
+      const data = {
+        "email": this.emailId.value,
+        "otp": this.verificationCode.value
+      }
+      this.authService.verifyOTP(data).subscribe((resp: any) => {
+        console.log("Response Verfy", resp);
+        if(resp?.statusCode === 200){
+          this.sharedService.showSuccess(resp.message);
+          this.verificationDisplay = false;
+          this.changePasswordDisplay = true;
+        }else{
+          this.sharedService.showError(resp.message);
+        }
+      },(e:any)=>{
+          this.sharedService.showError(e?.message);
+      });
+    }
+  }
+  changePassword() {
+    if (this.newPassword.valid && this.confirmPassword.valid && this.newPassword.value === this.confirmPassword.value) {
+      const data = {
+        "email": this.emailId.value,
+        "newPassword": this.newPassword.value,
+        "confirmPassword": this.newPassword.value
+      }
+      this.authService.resetPassword(data).subscribe((resp: any) => {
+        console.log("Response Changepawd", resp);
+        if(resp?.statusCode === 200){
+          this.sharedService.showSuccess(resp.message);
+          this.changePasswordDisplay = false;
+        }else{
+          this.sharedService.showError(resp.message);
+        }
+      },(e:any)=>{
+          this.sharedService.showError(e?.message);
+      });
+    }
+  }
+
   cancel() {
     this.display = false;
+  }
+  cancelVerification() {
+    this.verificationDisplay = false;
+  }
+  cancelPasswordChange() {
+    this.changePasswordDisplay = false;
   }
 }
