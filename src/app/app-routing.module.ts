2d9b5e7727be0dee758332c7a039d75e810b05a9
karthigeyan <karthiasokan179@gmail.com>
2024-09-12 18:31:12 +0530

    reset account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -52,7 +52,7 @@ const routes: Routes = [
     { path: 'grivence-form', component: AppErrorComponent },
     { path: 'access', component: AppAccessdeniedComponent },
     { path: 'notfound', component: AppNotfoundComponent },
-    { path: 'deative-account', component: RemoveAccountComponent },
+    { path: 'deactive-account', component: RemoveAccountComponent },
     { path: '**', redirectTo: 'notfound' },
 ]
 
